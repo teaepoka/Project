a04b4a8562a5f1d2f54cc976df1c3db7c536fa2c
Lois <loiskoka24@gmail.com>
2026-02-09 11:00:49 +0100

    Enhance UI styling in Role class
    Added styles to UI components for better appearance.

--- Role.java
+++ Role.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import core.*;
 
 public class Role {
@@ -12,40 +13,55 @@ public class Role {
 
         Label welcome = new Label("Welcome, " + user.getFullName());
         Button logout = new Button("Logout");
+        
+        welcome.setStyle("-fx-text-fill: #F7EC74;"+"-fx-font-weight: bold;"+"-fx-font-size: 30px;");
+        logout.setStyle("-fx-text-fill: #F5534B;"+"-fx-background-color: #F7EC74;"+"-fx-font-weight: bold;");
 
         VBox root = new VBox(10, welcome);
+        root.setStyle("-fx-background-color: #ED9E9A");
 
         if (user instanceof Listener) {
             TextField song = new TextField();
             Button play = new Button("Play Song");
+            
+            song.setStyle("-fx-background-color: #F5F3DC");
+            play.setStyle("-fx-text-fill: #F5534B;"+"-fx-background-color: #F7EC74;"+"-fx-font-weight: bold;");
 
             play.setOnAction(e ->
                 ((Listener) user).listenToSong(song.getText())
             );
+            Label ListenToSong = new Label();
+            ListenToSong.setStyle("-fx-text-fill: #F7EC74;"+"-fx-font-weight: bold;"+"-fx-font-size: 15px;");
 
-            root.getChildren().addAll(new Label("Listen to song:"), song, play);
+            root.getChildren().addAll(song,ListenToSong, play);
         }
 
         if (user instanceof Artist) {
             TextField song = new TextField();
             Button upload = new Button("Upload Song");
+            
+            song.setStyle("-fx-background-color: #F5F3DC");
+            upload.setStyle("-fx-text-fill: #F5534B;"+"-fx-background-color: #F7EC74;"+"-fx-font-weight: bold;");
 
             upload.setOnAction(e ->
                 ((Artist) user).uploadSong(song.getText())
             );
+            Label UploadSong = new Label();
+            UploadSong.setStyle("-fx-text-fill: #F7EC74;"+"-fx-font-weight: bold;"+"-fx-font-size: 15px;");
 
-            root.getChildren().addAll(new Label("Upload song:"), song, upload);
+            root.getChildren().addAll(song,UploadSong, upload);
         }
 
         if (user instanceof Admin) {
             Label adminLabel = new Label("Admin Dashboard");
+            adminLabel.setStyle("-fx-text-fill: #F7EC74;"+"-fx-font-weight: bold;"+"-fx-font-size: 15px;");
             root.getChildren().add(adminLabel);
         }
 
         logout.setOnAction(e -> Login.show(stage));
         root.getChildren().add(logout);
 
-        stage.setScene(new Scene(root, 400, 300));
+        stage.setScene(new Scene(root, 350, 250));
         stage.setTitle("Dashboard");
     }
 }
